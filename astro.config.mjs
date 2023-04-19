4efa92dd4ef51c844ade203888bc9777c5aeda17
tiwwwka <jolyfish23@gmail.com>
2023-04-19 11:09:05 +0200

    remove ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import react from '@astrojs/react'
 import netlify from '@astrojs/netlify/edge-functions';
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://main--stunning-cajeta-06aaca.netlify.app',
 	integrations: [tailwind(), react()],
-	output: 'server',
-  adapter: netlify(),
 })
