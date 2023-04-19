009020dd19268d6d9025cb9bafc18c340e62f90e
tiwwwka <jolyfish23@gmail.com>
2023-04-19 12:11:50 +0200

    add image optimize

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-// import image from '@astrojs/image'
+import image from '@astrojs/image'
 import react from '@astrojs/react'
 import netlify from '@astrojs/netlify/edge-functions';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://main--stunning-cajeta-06aaca.netlify.app',
-	integrations: [tailwind(), react()],
+	integrations: [tailwind(), react(), image()],
 })
