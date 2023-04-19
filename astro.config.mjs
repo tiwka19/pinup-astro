3ddcdf3e963dda86ea05c9bbc21c97c6ccba53a9
tiwwwka <jolyfish23@gmail.com>
2023-04-19 10:57:14 +0200

    add edge functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import image from '@astrojs/image'
+// import image from '@astrojs/image'
 import react from '@astrojs/react'
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify/edge-functions';
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react(), image()],
+	integrations: [tailwind(), react()],
 	output: 'server',
   adapter: netlify(),
 })
