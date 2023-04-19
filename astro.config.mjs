a135ded9a49ef706e16fcf5d38132b8f19fc70fc
tiwwwka <jolyfish23@gmail.com>
2023-04-19 23:42:39 +0200

    add ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import image from '@astrojs/image'
 import react from '@astrojs/react'
+
+import netlify from '@astrojs/netlify/edge-functions'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), react(), image()],
+	output: 'server',
+	adapter: netlify(),
 })
