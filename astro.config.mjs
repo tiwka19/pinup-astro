ceaf78b4dfb74d5b59408773b5a4ae0f833fc4aa
tiwwwka <jolyfish23@gmail.com>
2023-04-30 14:30:00 +0200

    add env & fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
-import image from '@astrojs/image'
-import netlify from '@astrojs/netlify/functions'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import image from '@astrojs/image';
+import netlify from '@astrojs/netlify/functions';
+import robotsTxt from 'astro-robots-txt';
+import sitemap from "@astrojs/sitemap";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react(), image()],
-	output: 'server',
-	adapter: netlify()
-})
+  integrations: [tailwind(), react(), image(), sitemap(), robotsTxt()],
+	site: 'https://motion-mat.net',
+  output: 'server',
+	adapter: node({
+    mode: 'standalone'
+  }),
+});
--- src/data/wordpress.ts
+++ src/data/wordpress.ts
@@ -1,21 +1,21 @@
 interface WPGraphQLParams {
-  query: string;
-  variables?: object;
+	query: string
+	variables?: object
 }
 
 export async function fetchAPI({ query, variables = {} }: WPGraphQLParams) {
-  const headers = { 'Content-Type': 'application/json' };
-  const res = await fetch('https://motion-mat.net/graphql', {
-    method: 'POST',
-    headers,
-    body: JSON.stringify({ query, variables }),
-  });
+	const headers = { 'Content-Type': 'application/json' }
+	const res = await fetch(import.meta.env.WORDPRESS_API_URL, {
+		method: 'POST',
+		headers,
+		body: JSON.stringify({ query, variables }),
+	})
 
-  const json = await res.json();
-  if (json.errors) {
-    console.log(json.errors);
-    throw new Error('Failed to fetch API');
-  }
+	const json = await res.json()
+	if (json.errors) {
+		console.log(json.errors)
+		throw new Error('Failed to fetch API')
+	}
 
-  return json.data;
+	return json.data
 }
